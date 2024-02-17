0bfc4d81aa90643c75dac809b8f1db32b040eead
Ilya Pugachev <ilyapugachv@gmail.com>
2024-02-17 15:01:59 +0300

    Project created

---
+++ RingSizeApp/RingSizeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RingSizeApp
+//
+//  Created by Илья Пугачёв on 17.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
