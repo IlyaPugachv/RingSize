d93d71ed360670b16ab16a42e0206e466c09b7ba
Ilya Pugachev <ilyapugachv@gmail.com>
2024-02-18 16:12:02 +0300

    TabBarController ring completed

--- RingSizeApp/RingSizeApp/Controller/RingVC.swift
+++ RingSizeApp/RingSizeApp/Controller/RingVC.swift
@@ -1,29 +1,31 @@
 import UIKit
 
-class RingVC: UIViewController {
+final class RingVC: UIViewController {
     
     private var circleLayer: CAShapeLayer?
     
-    private let infoCircle: UIImageView = {
-        let imageView = UIImageView()
-        let configuration = UIImage.SymbolConfiguration(pointSize: 24, weight: .regular)
-        let infoCircleImage = UIImage(systemName: "info.circle", withConfiguration: configuration)
-        imageView.image = infoCircleImage
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
+    //MARK: - LAZY CLOSURE
+    
+    lazy var infoCircle: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "info.circle"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
-    private let ringSize: UILabel = {
+    lazy var ringSize: UILabel = {
         let label = UILabel()
         label.text = "Ring Size"
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.font = .systemFont(ofSize: 16, weight: .medium)
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    private let textLabel: UILabel = {
+    lazy var textLabel: UILabel = {
         let label = UILabel()
         label.text = "Match the Ring to the Circle"
         label.textAlignment = .center
@@ -33,24 +35,25 @@ class RingVC: UIViewController {
         return label
     }()
     
-    private let gearCircle: UIImageView = {
-        let imageView = UIImageView()
-        let configuration = UIImage.SymbolConfiguration(pointSize: 24, weight: .regular)
-        let gearCircleImage = UIImage(systemName: "gearshape", withConfiguration: configuration)
-        imageView.image = gearCircleImage
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
+    lazy var gearCircle: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "gearshape"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
-    
-    private let slider: UISlider = {
+
+    lazy var slider: UISlider = {
         let slider = UISlider()
         slider.minimumValue = 12.04
         slider.maximumValue = 23.42
+        slider.minimumTrackTintColor = .orange
         slider.translatesAutoresizingMaskIntoConstraints = false
         return slider
     }()
     
-    private let plusBtn: UIButton = {
+    lazy var plusBtn: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "plus"), for: .normal)
         button.tintColor = .black
@@ -58,7 +61,7 @@ class RingVC: UIViewController {
         return button
     }()
     
-    private let minusBtn: UIButton = {
+    lazy var minusBtn: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "minus"), for: .normal)
         button.tintColor = .black
@@ -66,65 +69,133 @@ class RingVC: UIViewController {
         return button
     }()
     
-    private let radiusCircle: UILabel = {
+    lazy var sizeCircle: UILabel = {
         let label = UILabel()
         label.numberOfLines = 2
+        label.text = "17.73\nmm"
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    lazy var imageView: UIView = {
+        let image = UIView()
+        image.backgroundColor = UIColor.systemYellow.withAlphaComponent(0.3)
+        image.layer.cornerRadius = 18
+        image.layer.shadowColor = UIColor.black.cgColor
+        image.layer.shadowOpacity = 0.3
+        image.layer.shadowOffset = CGSize(width: 2, height: 2)
+        image.layer.shadowRadius = 4
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    lazy var unitsBtn: UIButton = {
+        let button = UIButton()
+        button.setTitle("AU/GB", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 18
+        button.backgroundColor = UIColor.brown.withAlphaComponent(0.3)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var defineRingBtn: UIButton = {
+        let button = UIButton()
+        button.setTitle("Define the Ring Size", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 25
+        button.contentEdgeInsets = UIEdgeInsets(top: 15, left: 20, bottom: 15, right: 20)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = CGSize(width: 2, height: 2)
+        button.layer.shadowRadius = 4
+        button.backgroundColor = UIColor.systemOrange
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
-        setupVStack()
+        setupUI()
+        slider.value = 17.73
+        sliderValueChanged(slider)
+    }
+    
+    //MARK: - PRIVATE FUNC
+    
+    private func setupUI() {
+        setupSubviews()
+        setupConstraint()
+        setupGestureSlider()
+        vsStackSliderValue()
+        vsStackRingSize()
+    }
+    
+    private func setupSubviews() {
+        let subviews = [unitsBtn, ringSize, textLabel, infoCircle, gearCircle, imageView, slider, plusBtn, minusBtn, sizeCircle, unitsBtn, defineRingBtn]
+        subviews.forEach { view.addSubview($0) }
+      
         createCircle(radius: 40)
     }
     
-    private func setup() {
-        view.addSubview(ringSize)
-        view.addSubview(textLabel)
-        view.addSubview(infoCircle)
-        view.addSubview(gearCircle)
-        view.addSubview(slider)
-        view.addSubview(plusBtn)
-        view.addSubview(minusBtn)
-        view.addSubview(radiusCircle)
-        
-        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
-        
-        NSLayoutConstraint.activate([
-            ringSize.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            ringSize.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        ])
-        
+    private func setupConstraint() {
         NSLayoutConstraint.activate([
             textLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
         
         NSLayoutConstraint.activate([
-            infoCircle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            infoCircle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+            sizeCircle.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 145),
+            sizeCircle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 194),
         ])
         
         NSLayoutConstraint.activate([
-            gearCircle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            gearCircle.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10)
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            imageView.bottomAnchor.constraint(lessThanOrEqualTo: slider.topAnchor, constant: -20),
+            imageView.heightAnchor.constraint(equalToConstant: 250)
         ])
         
         NSLayoutConstraint.activate([
-            slider.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 350),
-            slider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 65),
+            unitsBtn.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 110),
+            unitsBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 150),
+            unitsBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -150),
         ])
         
         NSLayoutConstraint.activate([
-            radiusCircle.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 235),
-            radiusCircle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 193),
+            defineRingBtn.topAnchor.constraint(equalTo: unitsBtn.bottomAnchor, constant: 35),
+            defineRingBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            defineRingBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
         ])
     }
     
-    private func setupVStack() {
+    private func setupGestureSlider() {
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+    }
+    
+    private func vsStackRingSize() {
+        let buttonsStack = UIStackView(arrangedSubviews: [infoCircle, ringSize, gearCircle])
+        buttonsStack.axis = .horizontal
+        buttonsStack.spacing = 10
+        buttonsStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsStack)
+        
+        NSLayoutConstraint.activate([
+            
+            buttonsStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            buttonsStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            buttonsStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            ringSize.centerYAnchor.constraint(equalTo: buttonsStack.centerYAnchor),
+            
+            infoCircle.widthAnchor.constraint(equalTo: infoCircle.heightAnchor),
+            gearCircle.widthAnchor.constraint(equalTo: gearCircle.heightAnchor),
+        ])
+    }
+    
+    private func vsStackSliderValue() {
         let buttonsStack = UIStackView(arrangedSubviews: [minusBtn, slider, plusBtn])
         buttonsStack.axis = .horizontal
         buttonsStack.spacing = 10
@@ -132,14 +203,14 @@ class RingVC: UIViewController {
         view.addSubview(buttonsStack)
         
         NSLayoutConstraint.activate([
-            buttonsStack.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 400),
-            buttonsStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 65),
-            buttonsStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -65)
+            buttonsStack.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 30),
+            buttonsStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 55),
+            buttonsStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -55)
         ])
     }
     
     private func createSegment(startAngle: CGFloat, endAngle: CGFloat, radius: CGFloat) -> UIBezierPath {
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: view.bounds.midX, y: view.bounds.midY),
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: view.bounds.midX, y: view.bounds.midY-90),
                                       radius: radius,
                                       startAngle: startAngle,
                                       endAngle: endAngle,
@@ -152,7 +223,7 @@ class RingVC: UIViewController {
         let segmentLayer = CAShapeLayer()
         segmentLayer.path = segmentPath.cgPath
         segmentLayer.lineWidth = 3
-        segmentLayer.strokeColor = UIColor.gray.cgColor
+        segmentLayer.strokeColor = UIColor.black.cgColor
         segmentLayer.fillColor = UIColor.clear.cgColor
         
         self.view.layer.addSublayer(segmentLayer)
@@ -160,9 +231,14 @@ class RingVC: UIViewController {
     }
     
     private func removeCircle() {
-        if let circleLayer = circleLayer {
-            circleLayer.removeFromSuperlayer()
-        }
+        guard let circleLayer = circleLayer else { return }
+        circleLayer.removeFromSuperlayer()
+    }
+    
+    private func roundedRadius(value: CGFloat) {
+        let radiusLabel = sizeCircle
+        let roundedRadius = String(format: "%.2f", slider.value)
+        radiusLabel.text = "\(roundedRadius) \nmm"
     }
     
     @objc private func sliderValueChanged(_ sender: UISlider) {
@@ -177,17 +253,9 @@ class RingVC: UIViewController {
         let maxRadius: CGFloat = 80
         let radius = min(maxRadius, max(minRadius, minRadius + ratio * (maxRadius - minRadius)))
         
-        let radiusLabel = radiusCircle
-        let roundedRadius = String(format: "%.2f", slider.value)
-            radiusLabel.text = "\(roundedRadius) \nmm"
-        
         removeCircle()
         createCircle(radius: radius)
+        roundedRadius(value: radius)
     }
 }
 
-extension CGFloat {
-    func toRadians() -> CGFloat {
-        return self * CGFloat(Double.pi) / 180.0
-    }
-}
---
+++ RingSizeApp/RingSizeApp/Extension/Extension.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension CGFloat {
+    func toRadians() -> CGFloat {
+        return self * CGFloat(Double.pi) / 180.0
+    }
+}
